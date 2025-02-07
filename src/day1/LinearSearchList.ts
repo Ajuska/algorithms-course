7647dfcccccf7a83d30765589a7d0e4f9e917fca
Andrea Vicarova <andrea.vicarova@gmail.com>
2025-02-07 16:13:27 -0500

    Add linear search solution

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,13 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-
+// also called greedy search
+// complexity is O(n) - running time increases, at most, linearly with the size of the items present in the list.
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle) {
+            return true;
+        }
+    }
+    return false;
 }
