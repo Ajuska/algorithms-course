deb14b5bf0b9bb7982f92752574ac7d01d943027
Andrea Vicarova <andrea.vicarova@gmail.com>
2025-02-07 22:21:16 -0500

    Add two crystal balls problem solution

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,24 @@
+// big o complexity: O(sqrt(n))
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let firstCrystalBall = jumpAmount;
 
+    for (; firstCrystalBall < breaks.length; firstCrystalBall += jumpAmount) {
+        if (breaks[firstCrystalBall]) {
+            break;
+        }
+    }
+
+    firstCrystalBall -= jumpAmount;
+    for (
+        let jump = 0;
+        jump < jumpAmount && jump < breaks.length;
+        jump++, firstCrystalBall++
+    ) {
+        if (breaks[firstCrystalBall]) {
+            return firstCrystalBall;
+        }
+    }
+
+    return -1;
 }
