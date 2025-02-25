bda2f168fcd76af88e5475d40dd504321e95462e
Andrea Vicarova <andrea.vicarova@gmail.com>
2025-02-25 13:16:44 +0100

    Add quick sort algorithm

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,34 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return;
+    }
+    const pivotIdx = partition(arr, lo, hi);
 
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+        }
+    }
+
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+    return idx;
+}
+
+// devide and conquer strategy
+// complexity: O(n log n), worst case (if the pivot is at the end of the array) O(n^2)
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
