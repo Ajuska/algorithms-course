6852fb6b7b395628e7040103a9fe2116d45b8605
Andrea Vicarova <andrea.vicarova@gmail.com>
2025-03-03 12:16:00 +0100

    Implement tree pre order search

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,24 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // base case
+    if (!curr) {
+        return path;
+    }
 
+    // pre
+    path.push(curr.value);
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // post - optionally do don't return anything here and in base case and just pass+return path variable in parent
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    // alternative when return type of walk is void
+    // const path: number[] = []
+    // walk(head, path)
+    // return path;
+    return walk(head, []);
 }
