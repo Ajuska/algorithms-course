583abc826fb7d2ffe74630fb17597a865e9c2716
Andrea Vicarova <andrea.vicarova@gmail.com>
2025-03-06 21:36:40 +0100

    Add depth first binary search

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+// complexity is O(log n) or worst case O(n) - depending on the hight of the tree and how balanced it is
+function search(current: BinaryNode<number> | null, needle: number): boolean {
+    if (!current) {
+        return false;
+    }
+    if (current.value === needle) {
+        return true;
+    }
 
+    if (current.value < needle) {
+        return search(current.right, needle);
+    }
+
+    return search(current.left, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
 }
